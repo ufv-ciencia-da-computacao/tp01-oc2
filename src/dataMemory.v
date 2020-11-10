e8ba89c5c408711703434fa70feec75bf2dc1b0a
Guilherme Melos <guilherme.melos@ufv.br>
2020-11-10 19:43:56 -0300

    sync mem_read

--- src/dataMemory.v
+++ src/dataMemory.v
@@ -7,18 +7,18 @@ module dataMemory(  input clk,
 
 	reg [7:0] memory [511:0];
 
-	if(mem_read) begin
-		assign read_data[7:0] <= memory[address];
-		assign read_data[15:8] <= memory[address + 1];
-		assign read_data[23:16] <= memory[address + 2];
-		assign read_data[31:24] <= memory[address + 3];
-		assign read_data[39:32] <= memory[address + 4];
-		assign read_data[47:40] <= memory[address + 5];
-		assign read_data[55:48] <= memory[address + 6];
-		assign read_data[63:56] <= memory[address + 7];
-	end
-	
+
 	always @(posedge clk) begin
+		if(mem_read == 1) begin
+			read_data[7:0] <= memory[address];
+			read_data[15:8] <= memory[address + 1];
+			read_data[23:16] <= memory[address + 2];
+			read_data[31:24] <= memory[address + 3];
+			read_data[39:32] <= memory[address + 4];
+			read_data[47:40] <= memory[address + 5];
+			read_data[55:48] <= memory[address + 6];
+			read_data[63:56] <= memory[address + 7];
+		end
 		if (mem_write == 1) begin
 			memory[address] <= write_data[7:0];
 			memory[address + 1] <= write_data[15:8];
