8b7ff7d0a40030ee069518722d70cba3cd69b457
Dener Vieira <deneribeiro10@hotmail.com>
2020-11-10 11:31:14 -0300

    add alu

--- src/TP_03.v
+++ src/TP_03.v
@@ -1,23 +1,26 @@
-module TP_03(	input clk, 
-					input [63:0] wdata, 
-					input flag, 
-					output [63:0] rd1, 
-					output [63:0] rd2);
+module TP_03(	input clk,
+					input [63:0] wdata,
+					input [3:0] op,
+					input flag,
+					output zero, 
+					output [63:0] result);
 	
 	wire [63:0] pc_value;
 	wire [63:0] new_pc_value;
 	wire [31:0] instruction;
+	wire [63:0] rd1;
+	wire [63:0] rd2;
 	
 	aluADD aluPC(pc_value, 4, new_pc_value);
 	progCounter PC(clk, new_pc_value, pc_value);
 	progMemory PM(pc_value, instruction);
-	registers regs(clk,
-						instruction[19:15], 
+	registers regs(clk, instruction[19:15], 
 						instruction[24:20], 
 						instruction[11:7], 
 						wdata, 
-						flag, 
+						flag,
 						rd1,
 						rd2);
+	aluOps alu(rd1, rd2, op, zero, result);
 	
 endmodule
---
+++ src/aluOps.v
@@ -0,0 +1,19 @@
+module aluOps(	input [63:0] data1,
+					input [63:0] data2,
+					input [3:0] op,
+					output zero, 
+					output reg [63:0] result);
+	
+	assign zero = (result == 0);
+	
+	always@(*) begin 
+		case(op)
+			4'b0000 : result <= data1 & data2;
+			4'b0001 : result <= data1 | data2;
+			4'b0010 : result <= data1 + data2;
+			4'b0110 : result <= data1 - data2;
+			default : result <= 64'b0;
+		endcase
+	end
+					
+endmodule
