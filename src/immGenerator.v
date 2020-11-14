de8b1ab2978e3ad8767c49e46ae0a512cae5ce5f
Guilherme Melos <guilherme.melos@ufv.br>
2020-11-14 00:37:19 -0300

    adding shiftLeft and immGen

--- src/immGenerator.v
+++ src/immGenerator.v
@@ -1,6 +1,22 @@
-module immGenerator(	input [31:0] instruction,
-							output [63:0] immediate);
+module immGenerator(
+             input [31:0] instruction,
+             output reg [63:0] immediate);
 
-		
+    integer i;
 
+	initial begin
+		for (i = 0 ; i <= 63; i = i + 1) immediate[i] = 0;
+	end
+	
+	always @ (*)
+	begin
+    	if(instruction[6:0] == 7'b0000011)
+			assign immediate[11:0] = instruction[31:20];
+
+		if(instruction[6:0]== 7'b0100011)
+		begin
+        	assign immediate[11:5] = instruction[31:25];
+        	assign immediate[4:0] = instruction[11:7];
+		end
+	end
 endmodule
--- src/shiftLeft.v
+++ src/shiftLeft.v
@@ -1,6 +1,10 @@
-module shiftLeft(	input [63:0] dataIn,
-						input [63:0] dataOut);
+module shiftLeft(    input [63:0] dataIn,
+                        output reg [63:0] dataOut);
+    integer i;
 
-
-						
+    initial begin
+        assign dataOut[0] = 0;
+        for (i = 62 ; i >= 0; i = i - 1)
+            assign dataOut[i+1] = dataIn[i];
+    end
 endmodule
