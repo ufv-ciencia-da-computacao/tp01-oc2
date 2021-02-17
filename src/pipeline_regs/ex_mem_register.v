ed09b145ea2614b0103e074fd65bdbb50f25d2c3
Germano Barcelos <gegenbarcelos@gmail.com>
2021-02-17 10:13:39 -0300

    control to mem_wb and ex_mem register

--- src/pipeline_regs/ex_mem_register.v
+++ src/pipeline_regs/ex_mem_register.v
@@ -5,6 +5,11 @@ module ex_mem_register (
     input [63:0] alu_result,
     input [31:0] wr,
     input [63:0] rd2,
+    input mem_read,
+    input mem_write,
+    input memToReg,
+    input branch,
+    input reg_write,
     input zero,
 
     output reg [63:0] pc_reg,
@@ -12,7 +17,12 @@ module ex_mem_register (
     output reg [63:0] alu_result_reg,
     output reg [31:0] wr_reg,
     output reg [63:0] rd2_reg,
-    output reg zero_reg,
+    output reg mem_read_reg,
+    output reg mem_write_reg,
+    output reg memToReg_reg,
+    output reg branch_reg,
+    output reg reg_write_reg,
+    output reg zero_reg
 );
 
     always @(posedge clk) begin
@@ -21,6 +31,11 @@ module ex_mem_register (
         alu_result_reg <= alu_result;
         wr_reg <= wr;
         rd2_reg <= rd2;
+        mem_read_reg <= mem_read;
+        mem_write_reg <= mem_write;
+        memToReg_reg <= memToReg;
+        branch_reg <= branch;
+        reg_write_reg <= reg_write;
         zero_reg <= zero;
     end
     
--- src/pipeline_regs/mem_wb_register.v
+++ src/pipeline_regs/mem_wb_register.v
@@ -2,16 +2,22 @@ module mem_wb_register (
     input clk,
     input [63:0] address,
     input [31:0] value,
-    input [31:0] wr,
+    input [31:0] rd,
+    input memToReg,
+    input regWrite,
 
     output reg [63:0] address_reg,
     output reg [31:0] value_reg,
     output reg [31:0] wr_reg,
+    output reg memToReg_reg,
+    output reg regWrite_reg,
 );
     
     always @(posedge clk) begin
         address_reg <= address;
         value_reg <= value;
         wr_reg <= wr;
+        memToReg_reg <= memToReg;
+        regWrite_reg <= regWrite;
     end
 endmodule
